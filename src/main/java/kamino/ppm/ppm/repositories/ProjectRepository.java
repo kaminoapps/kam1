98356357583ac23083d6c6f5a5737a333a7690ad
koloseum25 <36486398+koloseum25@users.noreply.github.com>
2019-01-23 21:55:32 +0100

    add project1

---
+++ src/main/java/kamino/ppm/ppm/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package kamino.ppm.ppm.repositories;
+
+import kamino.ppm.ppm.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+    @Override
+    default Iterable<Project> findAllById(Iterable<Long> iterable) {
+        return null;
+    }
+}
