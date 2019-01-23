0e48dea762b2d52a69248e8b57cdeb399f47e3f1
koloseum25 <36486398+koloseum25@users.noreply.github.com>
2019-01-23 20:55:43 +0100

    new

---
+++ src/main/java/kamino/ppm/ppm/PpmApplication.java
@@ -0,0 +1,14 @@
+package kamino.ppm.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmApplication.class, args);
+    }
+
+}
+
