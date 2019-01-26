10cd6f2365e22876f8a70a708bc1a4f4e2b0bd91
koloseum25 <36486398+koloseum25@users.noreply.github.com>
2019-01-26 18:05:24 +0100

    postman

--- src/main/java/kamino/ppm/ppm/services/ProjectService.java
+++ src/main/java/kamino/ppm/ppm/services/ProjectService.java
@@ -9,4 +9,10 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
+
+
+    public Project saveOrUpdateProject(Project project)
+    {
+        return  projectRepository.save(project);
+    }
 }
---
+++ src/main/java/kamino/ppm/ppm/web/ProjectController.java
@@ -0,0 +1,27 @@
+package kamino.ppm.ppm.web;
+
+
+import kamino.ppm.ppm.domain.Project;
+import kamino.ppm.ppm.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
